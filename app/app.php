f0df684833cb769ec197e6e858ed79befb5597d0
Mark Lawson <mlawson3691@gmail.com>
2016-09-09 09:00:01 -0700

    Add reference to twig template for root path

--- app/app.php
+++ app/app.php
@@ -5,13 +5,16 @@
     session_start();
     if (empty($_SESSION['list_of_contacts'])) {
         $_SESSION['list_of_contacts'] = array();
+        $person = new Contact('Bob', '111-111-1111', '111 One Lane Portland, OR 11111');
+        $person->save();
     }
 
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
-    $app->get('/', function() {
-        return "Home";
+    $app->get('/', function() use ($app) {
+        $allContacts = Contact::getAll();
+        return $app['twig']->render('contacts.html.twig', array('contacts' => $allContacts));
     });
 
     return $app;
