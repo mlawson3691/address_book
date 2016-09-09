c3cdc9c20f7ac79e8f42d5839063481409970d7d
Mark Lawson <mlawson3691@gmail.com>
2016-09-09 08:41:20 -0700

    Create a session array to store all contacts

--- app/app.php
+++ app/app.php
@@ -3,6 +3,9 @@
     require_once __DIR__.'/../src/Contact.php';
 
     session_start();
+    if (empty($_SESSION['list_of_contacts'])) {
+        $_SESSION['list_of_contacts'] = array();
+    }
 
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
