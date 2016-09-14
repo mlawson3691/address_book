9bd39cb1cfcaf17a2c0aad7d03296489c9412fff
Mark Lawson <mlawson3691@gmail.com>
2016-09-13 22:13:25 -0700

    Fix display/hide of new contact form and related buttons

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -3,5 +3,12 @@ $(function() {
     $('#newContact').click(function() {
         $('.form-create').slideDown();
         $(this).hide();
+        $('#hideNewContact').show();
+    });
+
+    $('#hideNewContact').click(function() {
+        $('.form-create').slideUp();
+        $(this).hide();
+        $('#newContact').show();
     });
 });
